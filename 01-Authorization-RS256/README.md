1463237425f5b0121777a2ce352b36bd0b53fcf8
Ryan Chenkie <ryanchenkie@gmail.com>
2017-05-09 09:54:09 -0400

    use access token and RS256

---
+++ 01-Authorization-RS256/README.md
@@ -0,0 +1,45 @@
+# Auth0 Falcor RS256 Authorization
+
+## Install the Dependencies
+
+Run `npm install` to ensure local dependencies are available.
+
+Rename the `.env.example` file to `.env`. This file has two values, `AUTH0_AUDIENCE` and `AUTH0_DOMAIN`. If you download this sample from the quickstart page, the value for `AUTH0_DOMAIN` will be populated automatically, but you will still need to populate `AUTH0_AUDIENCE` manually. The value for `AUTH0_AUDIENCE` is the identifier used for an API that you create in the Auth0 dashboard.
+
+## Run the Application
+
+The development server that comes with create-react-app can be used to serve the application.
+
+```bash
+npm start
+```
+
+The application will be served at `http://localhost:3001`.
+
+## What is Auth0?
+
+Auth0 helps you to:
+
+* Add authentication with [multiple authentication sources](https://docs.auth0.com/identityproviders), either social like **Google, Facebook, Microsoft Account, LinkedIn, GitHub, Twitter, Box, Salesforce, amont others**, or enterprise identity systems like **Windows Azure AD, Google Apps, Active Directory, ADFS or any SAML Identity Provider**.
+* Add authentication through more traditional **[username/password databases](https://docs.auth0.com/mysql-connection-tutorial)**.
+* Add support for **[linking different user accounts](https://docs.auth0.com/link-accounts)** with the same user.
+* Support for generating signed [Json Web Tokens](https://docs.auth0.com/jwt) to call your APIs and **flow the user identity** securely.
+* Analytics of how, when and where users are logging in.
+* Pull data from other sources and add it to the user profile, through [JavaScript rules](https://docs.auth0.com/rules).
+
+## Create a free Auth0 account
+
+1. Go to [Auth0](https://auth0.com/signup) and click Sign Up.
+2. Use Google, GitHub or Microsoft Account to login.
+
+## Issue Reporting
+
+If you have found a bug or if you have a feature request, please report them at this repository issues section. Please do not report security vulnerabilities on the public GitHub issue tracker. The [Responsible Disclosure Program](https://auth0.com/whitehat) details the procedure for disclosing security issues.
+
+## Author
+
+[Auth0](auth0.com)
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE.txt) file for more info.
---
+++ 01-Authorization-RS256/server.js
@@ -0,0 +1,85 @@
+const express       = require('express');
+const falcorExpress = require('falcor-express');
+const Router        = require('falcor-router');
+const jwt           = require('express-jwt');
+const dotenv        = require('dotenv');
+const bodyParser    = require('body-parser');
+const jwtAuthz      = require('express-jwt-authz');
+const jwksRsa       = require('jwks-rsa');
+const cors          = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('.'));
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
+}
+
+app.use(cors());
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+const checkScopes = jwtAuthz([ 'read:messages' ]);
+
+var data = [
+  {
+    "name": "AngularJS",
+    "language": "JavaScript"
+  },
+  {
+    "name": "NodeJS",
+    "language": "JavaScript"
+  },
+  {
+    "name": "Laravel",
+    "language": "PHP"
+  }
+];
+
+app.use('/api/model.json', checkJwt, checkScopes, falcorExpress.dataSourceRoute(function(req, res) {
+  return new Router([
+    {
+      route: "frameworks[{integers:frameworkIds}]['name', 'language']",
+      get: function(pathSet) {
+        var results = [];
+
+        pathSet.frameworkIds.forEach(function(frameworkId) {
+
+          // An array of key names that map is held at pathSet[2]
+          pathSet[2].map(function(key) {
+
+            // Find the framework that corresponds to the current frameworkId
+            var frameworkRecord = data[frameworkId];
+
+            // Finally we push a path/value object onto
+            // the results array
+            results.push({
+              path: ['frameworks', frameworkId, key], 
+              value: frameworkRecord[key]
+            });
+          });          
+        });
+
+        return results;
+      }
+    }
+  ]);
+}));
+
+app.listen(3001);
+console.log("Listening on http://localhost:3001");
